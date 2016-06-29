85961ed5633cb258f5d7eb0ac4438b858b62b5e0
AgelxNash <agel_nash@xaker.ru>
2016-06-29 13:51:58 +0500

    Disabling the line cutting. Warnings only.

--- public/js/FormItems/seo.js
+++ public/js/FormItems/seo.js
@@ -2,15 +2,14 @@
     $.fn.limitlength = function(length) {
 
         $(this).after('<div class="informer"></div>');
-        var informer = this.next('.informer');
+        var informer = this.next('.informer'),
+            remaining = length -  $(this).val().length;
         informer.css('font-size','11px');
-        informer.append("Осталось <strong>"+  length+"</strong> символов");
+        informer.append((remaining > 0 ? "Осталось" : "Лишних") + " <strong>"+  Math.abs(remaining) + "</strong> символов");
+        if(remaining <= 10) { informer.css("color","red"); }
         $(this).on('keyup',function(event){
-            if($(this).val().length > length){
-                $(this).val($(this).val().substr(0, length));
-            }
             var remaining = length -  $(this).val().length;
-            informer.html("Осталось <strong>"+  remaining+"</strong> символов");
+            informer.html((remaining > 0 ? "Осталось" : "Лишних") + " <strong>"+  Math.abs(remaining) + "</strong> символов");
             if(remaining <= 10) { informer.css("color","red"); }
             else {
                 event.preventDefault();
